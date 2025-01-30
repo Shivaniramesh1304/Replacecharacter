1d5af51b8746b0e5f8380ee1d58ee56311cd11d4
Shivaniramesh1304 <shivaniramesh1304@gmail.com>
2025-01-30 21:55:20 +0530

    Add files via upload

---
+++ Replacecharacter.java
@@ -0,0 +1,7 @@
+public class Replacecharacter {
+    public static void main(String[] args) {
+        String str="hello world";
+        String modifiedStr=str.replace('o' ,'x');
+        System.out.println(modifiedStr);
+    }
+} 
